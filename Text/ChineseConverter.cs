c850e55649db535861370d9dacafef4a8076dcb4
Ken Sze <Ken.Sze@bsgroup.com.hk>
2017-11-01 11:48:31 +0800

    Initial

---
+++ Text/ChineseConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Pickup.Text
+{
+    public class ChineseConverter
+    {
+
+        private const int LocaleSystemDefault = 0x0800;
+        private const int LcmapSimplifiedChinese = 0x02000000;
+        private const int LcmapTraditionalChinese = 0x04000000;
+
+        [DllImport("kernel32", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern int LCMapString(int locale, int dwMapFlags, string lpSrcStr, int cchSrc,
+                                      [Out] string lpDestStr, int cchDest);
+
+        /// <summary>
+        /// 繁体转简体。符號可能被轉成全型
+        /// </summary>
+        /// <param name="argSource"></param>
+        /// <returns></returns>
+        public static string ToSimplified(string argSource)
+        {
+            var t = new String(' ', argSource.Length);
+            LCMapString(LocaleSystemDefault, LcmapSimplifiedChinese, argSource, argSource.Length, t, argSource.Length);
+            return t;
+        }
+
+        /// <summary>
+        /// 简体转繁体 。符號可能被轉成全型
+        /// </summary>
+        /// <param name="argSource"></param>
+        /// <returns></returns>
+        public static string ToTraditional(string argSource)
+        {
+            var t = new String(' ', argSource.Length);
+            LCMapString(LocaleSystemDefault, LcmapTraditionalChinese, argSource, argSource.Length, t, argSource.Length);
+            return t;
+        }
+    }
+}
---
+++ Text/VBChineseConverter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+
+//ref: http://www.cnblogs.com/Anna2Serena/archive/2011/10/13/2210235.html
+namespace Pickup.Text
+{
+    public class VBChineseConverter
+    {
+
+        /// <summary>
+        /// 繁体转简体
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static string ToSimplified(string s)
+        {
+            return Strings.StrConv(s, VbStrConv.SimplifiedChinese, 0);
+        }
+
+        /// <summary>
+        /// 简体转繁体
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static string ToTraditional(string s)
+        {
+            return Strings.StrConv(s, VbStrConv.TraditionalChinese, 0);
+        }
+
+        /// <summary>
+        /// 半角转全角
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static string ToWideChar(string s)
+        {
+            return Strings.StrConv(s, VbStrConv.Wide, 0);
+        }
+
+        /// <summary>
+        /// 全角转半角
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static string ToNarrow(string s)
+        {
+            return Strings.StrConv(s, VbStrConv.Narrow,0 );
+        }
+    }
+}
